a9469820062b23f97bf8af579d89f9f1d20dab91
Avanti1729 <avantikagupta1729@gmail.com>
2026-06-02 21:51:35 +0530

    phase-8: add terraform iac for vpc, ecr, and eks

---
+++ terraform/eks.tf
@@ -0,0 +1,31 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"
+
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.27"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
